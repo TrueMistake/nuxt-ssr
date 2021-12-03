feace21072bcc505e2cdb5954566faaaeb4a0309
TrueMistake <ooghon@gmail.com>
2021-12-03 13:17:03 +0300

    Add files via upload
    init

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- navigation -->
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-start">
+          <nuxt-link to="/" class="navbar-item">
+            Awesome JS SSR Blog!
+          </nuxt-link>
+          <nuxt-link active-class="is-active" to="/" class="navbar-item is-tab" exact>Home</nuxt-link>
+          <nuxt-link active-class="is-active" to="/about" class="navbar-item is-tab" exact>About</nuxt-link>
+        </div>
+      </div>
+    </nav>
+    <!-- /navigation -->
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class="hero-content">
+            <h1 class="title">
+              Welcome to the JavaScript SSR Blog.
+            </h1>
+            <h2 class="subtitle">
+              Hope you find something you like.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <posts />
+  </div>
+</template>
+
+<script>
+import Posts from '~/components/Posts'
+export default {
+  head: {
+    title: 'Home'
+  },
+  components:{
+    Posts
+  }
+}
+</script>
+
+<style>
+.hero-body{
+  padding: 0;
+}
+.hero > .container {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  line-height: 58px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 32px;
+  line-height: 38px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.main-content{
+  padding: 2rem;
+}
+</style>
+
